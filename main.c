eecc04b460fedced23d7ed792cb3315da3cfde75
Navneet333 <106150179+Navneet333@users.noreply.github.com>
2023-08-31 16:05:21 +0530

    Update main.c
    this is navneet changes

--- main.c
+++ main.c
@@ -1,7 +1,8 @@
 #include<stdio.h>
 int main() {
     int number1,number2,sum;
-
+    // this code is written by jay anjana
+    
     printf("enter the numbers:");
     scanf("%d %d",&number1,&number2);
 
